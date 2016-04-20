ebe10adb19799b2fd3a0b895bfde95b4afaa9492
LAVRENKO <lavrenko.iaroslav@gmail.com>
2016-04-20 20:10:40 +0200

    hfkjgdshkjghkjsfg

---
+++ src/Runner.java
@@ -0,0 +1,14 @@
+/**
+ * Created by LAVRENKO on 20.04.2016.
+ */
+public class Runner {
+
+    public static void main(String[] args) {
+        int a=4;
+        int b=2;
+
+        int mul = Multiplicator.mul(a, b);
+        //Multiplicator.add
+        System.out.println(mul);
+    }
+}
